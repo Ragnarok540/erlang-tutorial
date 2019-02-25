022c5db8b0b1ef1328b6ba75e115025e3d112f0a
Edgar Nova <ragnarok540@gmail.com>
2019-02-24 20:17:39 -0500

    refactor chat

--- src/chat_client.erl
+++ src/chat_client.erl
@@ -2,5 +2,18 @@
 
 -compile(export_all).
 
-send_message(RouterPid, Addressee, MessageBody) ->
-    message_router:send_chat_message(RouterPid, Addressee, MessageBody).
+register_client(ClientName) ->
+    message_router:register_client(ClientName, fun(MessageBody) -> 
+        chat_client:print_message(ClientName, MessageBody) end).
+
+unregister_client(ClientName) ->
+    message_router:unregister_client(ClientName).
+
+send_message(Addressee, MessageBody) ->
+    message_router:send_chat_message(Addressee, MessageBody).
+
+print_message(ClientName, MessageBody) ->
+    io:format("~p received: ~p~n", [ClientName, MessageBody]).
+
+start_router() ->
+    message_router:start().
