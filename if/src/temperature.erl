c64e2f06fc392c6b6093211a9f6fc0c8d05e1967
Edgar Nova <ragnarok540@gmail.com>
2024-09-15 10:35:32 -0500

    If

---
+++ if/src/temperature.erl
@@ -0,0 +1,12 @@
+-module(temperature).
+-export([check_temperature/1]).
+
+check_temperature(Temperature) ->
+    if
+        Temperature < 40 ->
+            io:format("Temperature is ok~n");
+        Temperature > 40 ->
+            io:format("It's getting hot~n");
+        true ->
+            io:format("Any other case!~n")
+    end.
