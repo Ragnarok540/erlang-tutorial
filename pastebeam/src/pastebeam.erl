c2a6db15c2e6581fce6dbdcb411c0baaf06fa5ff
Edgar Nova <ragnarok540@gmail.com>
2025-10-12 16:08:06 -0500

    challenge state

--- pastebeam/src/pastebeam.erl
+++ pastebeam/src/pastebeam.erl
@@ -6,8 +6,17 @@ start() ->
     Accepter = spawn(?MODULE, accepter, [LSock]),
     Accepter.
 
+fail_session(Sock, Reason) ->
+    io:format("ERROR: session failed: ~w\n", [Reason]),
+    gen_tcp:close(Sock),
+    ok.
+
 -spec session(State, Sock) -> ok when
-    State :: command | {challenge, list(binary())} | {post, list(binary())} | {get, unicode:chardata()},
+    State :: command | 
+             {challenge, list(binary())} |
+             {accepted, list(binary()), binary()} |
+             {post, list(binary())} |
+             {get, unicode:chardata()},
     Sock :: get_tcp:socket().
 
 session(command, Sock) ->
@@ -21,9 +30,7 @@ session(command, Sock) ->
             gen_tcp:close(Sock),
             ok;
         {error, Reason} ->
-            io:format("ERROR: session failed: ~w\n", [Reason]),
-            gen_tcp:close(Sock),
-            ok
+            fail_session(Sock, Reason)
     end;
 session({post, Lines}, Sock) ->
     case gen_tcp:recv(Sock, 0) of
@@ -32,16 +39,25 @@ session({post, Lines}, Sock) ->
         {ok, Line} ->
             session({post, [Line|Lines]}, Sock);
         {error, Reason} ->
-            io:format("ERROR: session failed: ~w\n", [Reason]),
+            fail_session(Sock, Reason)
+    end;
+session({challenge, Lines}, Sock) ->
+    Challenge = binary:encode_hex(crypto:strong_rand_bytes(32)),
+    gen_tcp:send(Sock, [<<"CHALLENGE: ">>, Challenge, <<"\r\n">>]),
+    session({accepted, Lines, Challenge}, Sock);
+session({accepted, Lines, Challenge}, Sock) ->
+    case gen_tcp:recv(Sock, 0) of
+        {ok, <<"ACCEPTED ", Id/binary>>} ->
+            'TODO';
+        {ok, _} ->
+            gen_tcp:send(Sock, "INVALID COMMAND\r\n"),
             gen_tcp:close(Sock),
-            ok
+            ok;
+        {error, Reason} ->
+            fail_session(Sock, Reason)
     end;
 session({get, Id}, Sock) ->
     io:format("GET ~s\n", [Id]),
-    gen_tcp:close(Sock),
-    ok;
-session({challenge, Lines}, Sock) ->
-    io:format("CHALLENGE ~w\n", [Lines]),
     gen_tcp:close(Sock),
     ok.
 
